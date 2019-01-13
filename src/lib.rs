52a1ccc735bd979db1de882a30c8a6afac037dae
VentGrey <omarpurataf@gmail.com>
2019-01-13 02:57:04 -0600

    Added FizzBuzz function

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,14 @@ pub fn basic_op(op: char, value1: i32, value2: i32) -> i32 {
         _ => panic!("Invalid operator"),
     }
 }
+
+pub fn fizzbuzz(iter: i32) {
+    for x in 0..=iter {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            (_, _) => println!("{}", x),
+        }
+    }
+}
